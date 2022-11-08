04e96a8983413886e08d74e35cdb1ee6c204aeaa
Iceomatic <89707822+iceomatic@users.noreply.github.com>
2022-11-08 17:31:16 +0000

    hw

--- homeworks_rust/homeworks/homework7/error_handling/errors2.rs
+++ homeworks_rust/homeworks/homework7/error_handling/errors2.rs
@@ -16,16 +16,19 @@
 // There are at least two ways to implement this that are both correct-- but
 // one is a lot shorter! Execute `rustlings hint errors2` for hints to both ways.
 
-// I AM NOT DONE
 
-use std::num::ParseIntError;
 
-pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
-    let processing_fee = 1;
-    let cost_per_item = 5;
-    let qty = item_quantity.parse::<i32>();
 
-    Ok(qty * cost_per_item + processing_fee)
+
+pub fn total_cost(item_quantity: &str) -> Result<i32, String> {
+    match item_quantity.parse::<i32>(){
+        Ok(_) => {let processing_fee = 1;
+                    let cost_per_item = 5;
+                    let qty = item_quantity.parse::<i32>().unwrap();
+
+                    Ok(qty * cost_per_item + processing_fee)},
+        _ => Err("invalid digit found in string".to_string())
+    }
 }
 
 #[cfg(test)]
